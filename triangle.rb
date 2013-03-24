ffd0441ae86effe37603dcef43a9ca70cc77d8ef
Matthew Colson <colson78@live.missouristate.edu>
2013-03-23 22:05:29 -0500

    update about_triangle_project_2 to pass test

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,20 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-	if(a == b && a ==c)
+
+	if (a <= 0) || (b <= 0) || (c <= 0)
+		raise TriangleError, 
+			"Sides must by numbers greater than zero"
+	end
+
+	if (a+b <= c) || (a+c <= b) || (b+c <= a)
+		raise TriangleError, 
+			"No two sides can add to be less than or equal to the other side" 
+	end
+
+	if a == b && a ==c
 		:equilateral
-	elsif(a == b || a == c || b == c)
+	elsif(a == b && a != c)  || (a == c && a != b) || (b == c && b != a)
 		:isosceles
 	else
 		:scalene
